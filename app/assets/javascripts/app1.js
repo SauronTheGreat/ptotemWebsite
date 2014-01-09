1ed952b1f9c4609ef1a14e2d024c93b22bd56e0e
The dark Lord <contactme@rushabhhathi.com>
2014-01-09 11:32:47 +0530

    admin

---
+++ app/assets/javascripts/app1.js
@@ -0,0 +1,11 @@
+//= require jquery
+
+//= require jquery.easing.1.3
+//= require bootstrap.min
+//= require jquery.parallax.min
+//= require jquerypp.min
+//= require jquery.bookblock.min
+//= require jquery.windy
+//= require mobileview.js
+//= require jquery.knob.js
+//= require bootbox.min
